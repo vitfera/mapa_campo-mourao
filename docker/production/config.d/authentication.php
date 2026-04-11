54ce5657c1b0a9dd8a16e61ad0a9a70bf6af3c4d
Victor Ferreira <vitfera@hotmail.com>
2026-04-11 14:51:33 -0300

    ativando login via google

--- docker/production/config.d/authentication.php
+++ docker/production/config.d/authentication.php
@@ -25,6 +25,7 @@ return [
                 'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
             ),
             'Google' => array(
+                'visible' => true,
                 'client_id' => env('AUTH_GOOGLE_CLIENT_ID', true),
                 'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
